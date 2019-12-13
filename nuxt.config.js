f3a429d80babdd774ca8f2b5f6c4ca1e3d60c064
StefKors <stef.kors@gmail.com>
2019-12-14 00:18:37 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: 'oneacre #1',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
