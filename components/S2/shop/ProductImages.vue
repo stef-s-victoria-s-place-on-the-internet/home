f4523db5c3213c121edf5cb3ae6513282749013c
Titus Knegtel <info@titusknegtel.nl>
2020-11-14 22:31:21 +0100

    Added images - Concrete Flux

---
+++ components/S2/shop/ProductImages.vue
@@ -0,0 +1,38 @@
+<template>
+<section class="product-images">
+  <slot />
+</section>
+</template>
+
+<script>
+export default {
+  name: 'ProductImages',
+}
+</script>
+
+<style lang="scss" scoped>
+.product {
+    &-images {
+
+        .image-content {
+            display: grid;
+            grid-auto-rows: auto;
+            grid-gap: 1rem;
+        }
+        figure {
+            width: 100%;
+            margin: 0;
+            background: $grey;
+
+            &:nth-of-type(even) {
+                justify-self: flex-end;
+            }
+
+            img {
+                width: 100%;
+                display: block;
+            }
+        }
+    }
+}
+</style>
