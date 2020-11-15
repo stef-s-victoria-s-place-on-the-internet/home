5886527c1bdd2c5f79524c5d8e026b034b87866d
StefKors <stef.kors@gmail.com>
2020-11-15 18:50:28 +0100

    add placeholder content

--- pages/more/success.vue
+++ pages/more/success.vue
@@ -16,6 +16,8 @@ export default {
         id: query.id,
       })
 
+      console.log(customer)
+
       if (!customer) {
         return {
           customer: {
@@ -55,6 +57,39 @@ export default {
         customer,
       }
     } catch (err) {
+      return {
+        customer: {
+          _id: '5fb164cefbd53725b03dd7d0',
+          id: 'D1605461198533',
+          name: 'Stef Kors',
+          email: 'stef.kors@gmail.com',
+          adress: 'Tenierstraat 13C',
+          postalcode: '2526NX',
+          country: {
+            iso: 'NL',
+            name: 'Netherlands',
+          },
+          date: '2020-11-15T17:26:38.533Z',
+          paid: true,
+          invoicenr: 28,
+          payment_amount: '16.10',
+          products: [
+            {
+              name: 'radio',
+              price: 5,
+            },
+            {
+              name: 'techno',
+              price: 5,
+            },
+          ],
+          attachment: {
+            name: 'SHOP-OA28-D1605461198533.pdf',
+            path: './temp_files/SHOP-OA28-D1605461198533.pdf',
+          },
+        },
+      }
+
       console.log(err)
     }
   },
