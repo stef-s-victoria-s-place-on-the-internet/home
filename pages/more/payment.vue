fd651d0d222fd9569a958caabec037c09a8b561a
Titus Knegtel <info@titusknegtel.nl>
2020-11-15 17:07:39 +0100

    Payment layout fix

--- pages/more/payment.vue
+++ pages/more/payment.vue
@@ -1,49 +1,33 @@
 <template>
-  <div class="testing">
+<div class="shop-wrapper">
+
+  <LogoCircle />
+
+  <div class="form-content">
+    <h1>Customer details</h1>
     <Form debug>
-      <FormItem
-        v-bind:label="form.name.label"
-        v-bind:value.sync="form.name.value"
-        v-bind:type="form.name.type"
-      />
+      <FormItem v-bind:label="form.name.label" v-bind:value.sync="form.name.value" v-bind:type="form.name.type" />
       <FormItem v-bind:label="form.country.label">
-        <v-select
-          label="name"
-          v-model="form.country.value"
-          :options="countries"
-        />
+        <v-select label="name" v-model="form.country.value" :options="countries" />
       </FormItem>
-      <FormItem
-        v-bind:label="form.email.label"
-        v-bind:value.sync="form.email.value"
-        v-bind:type="form.email.type"
-      />
-      <FormItem
-        v-bind:label="form.adress.label"
-        v-bind:value.sync="form.adress.value"
-        v-bind:type="form.adress.type"
-      />
-      <FormItem
-        v-bind:label="form.postalcode.label"
-        v-bind:value.sync="form.postalcode.value"
-        v-bind:type="form.postalcode.type"
-      />
+      <FormItem v-bind:label="form.email.label" v-bind:value.sync="form.email.value" v-bind:type="form.email.type" />
+      <FormItem v-bind:label="form.adress.label" v-bind:value.sync="form.adress.value" v-bind:type="form.adress.type" />
+      <FormItem v-bind:label="form.postalcode.label" v-bind:value.sync="form.postalcode.value" v-bind:type="form.postalcode.type" />
     </Form>
-    <ProductList
-      :products="products"
-      :country="form.country"
-      :shippingRates="rates"
-    />
+    <ProductList :products="products" :country="form.country" :shippingRates="rates" />
   </div>
+</div>
 </template>
 
 <script>
+import LogoCircle from '~/components/S2/identity/LogoCircle.vue'
 import ProductList from '~/components/S2/shop/ProductList'
 import FormItem from '~/components/S2/shop/FormItem'
 import Form from '~/components/S2/shop/Form'
 
 export default {
   components: {
+    LogoCircle,
     Form,
     FormItem,
     ProductList,
@@ -82,8 +66,7 @@ export default {
       },
       countries: [],
       rates: {},
-      products: [
-        {
+      products: [{
           name: 'radio',
           price: 5,
         },
@@ -94,7 +77,9 @@ export default {
       ],
     }
   },
-  async asyncData({ $axios }) {
+  async asyncData({
+    $axios
+  }) {
     try {
       const countries = await $axios.$get('/shop/shipping/countries')
       const rates = await $axios.$get('/shop/shipping/rates')
@@ -110,15 +95,79 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.testing {
-  padding: 1rem;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 1rem;
+.shop-wrapper {
+    width: 100%;
+    max-width: 1600px;
+    padding: 2rem 2rem 4rem;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 15rem minmax(min-content, 80rem);
+    grid-gap: 4rem;
+    align-items: flex-start;
+    justify-content: center;
+    @include respond-until($screen-lg) {
+        grid-gap: 3rem;
+    }
+    @include respond-until($screen-md) {
+        grid-template-columns: 12.5rem minmax(min-content, 80rem);
+    }
+    @include respond-until($screen-sm) {
+        grid-template-columns: 1fr;
+        padding: 0 2rem 10rem;
+    }
+    @include respond-until($screen-xs) {
+        padding: 0 1rem 10rem;
+    }
 
-  & > * {
-    border: 1px solid;
-    padding: 1rem;
-  }
+    .shop-side {
+        height: calc(100vh - 2rem);
+        position: sticky;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        top: 2rem;
+        padding: 0 0 2rem;
+        @include respond-until($screen-sm) {
+            top: 0;
+            height: auto;
+        }
+    }
+
+    h1 {
+        width: 100%;
+        font-size: 6rem;
+        font-family: $font-nova;
+        line-height: 1em;
+        padding-bottom: 4rem;
+        text-transform: lowercase;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        margin: 0;
+        @include respond-until($screen-lg) {
+            font-size: 5.5rem;
+        }
+        @include respond-until($screen-md) {
+            font-size: 5rem;
+        }
+        @include respond-until($screen-sm) {
+            width: 60%;
+            padding-bottom: 0;
+        }
+        @include respond-until($screen-xs) {
+            font-size: 4rem;
+        }
+    }
+    .form-content {
+        padding: 1rem;
+        display: grid;
+        grid-auto-rows: auto;
+        grid-gap: 4rem;
+        border-top: 1px solid;
+        @include respond-until($screen-sm) {
+            border-top: 0;
+            padding-top: 10vh;
+        }
+    }
 }
 </style>
