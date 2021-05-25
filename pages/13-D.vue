3acb3c94fa3a1b37fa84a549f6d63574bc4882df
Stef Kors <stef.kors@gmail.com>
2021-05-26 00:14:23 +0200

    fix

--- pages/13-D.vue
+++ pages/13-D.vue
@@ -96,6 +96,24 @@
 <script>
 export default {
   layout: '13D',
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content:
+            'https://www.stefsandvictoriasplaceontheinternet.nl/postcard_stamp.jpg',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content:
+            'https://www.stefsandvictoriasplaceontheinternet.nl/postcard_stamp.jpg',
+        },
+      ],
+    }
+  },
   computed: {
     availableLocales() {
       return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
